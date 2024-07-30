f4fa2795b587e3139e3d17f54b8c8fb079f37787
Sasha <a.kleutin@msocialproduction.com>
2024-07-30 22:21:34 +0300

    #add #backend - basic migrations

--- backend/database/migrations/2024_07_30_190407_create_payments_table.php
+++ backend/database/migrations/2024_07_30_190407_create_payments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users', 'id')->onDelete('cascade');
             $table->string('payload');
             $table->decimal('amount');
             $table->char('currency_iso_code', 3);
